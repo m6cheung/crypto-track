9e5e9f2f1db451558642e64311eca18a7b2b5b2d
Michael Cheung <michaelc1993@gmail.com>
2018-05-02 02:55:52 -0700

    serve static files from express server.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
-import express from 'express';  
-import logger from 'morgan';
-const app = express();
+const express = require('express'),  
+      logger = require('morgan'),
+      config = require('./config/'),
+      path = require('path'),
+      app = express();
+
+app.use(logger('dev'));
+
+app.use(function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../build')))
+const server = app.listen(config.port);
+console.log("server running on port: " + config.port);
